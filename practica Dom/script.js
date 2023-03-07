67df0e6aad7257061c22e789d4bb333804989ff4
darckis28 <114121428+darckis28@users.noreply.github.com>
2023-03-07 17:35:38 -0500

    Add files via upload

---
+++ practica Dom/script.js
@@ -0,0 +1,27 @@
+
+import checkComplete from "./componentes/checkComplete.js";
+import deleteIcon from "./componentes/deleteIcon.js"
+
+const btn = document.querySelector("[data-form-btn]");
+const createTask =(e)=>{
+    e.preventDefault()
+    const input = document.querySelector("[data-form-input]");
+    const value= input.value;
+    const list = document.querySelector("[data-list]")
+    const task = document.createElement("li");
+    task.classList.add("card")
+    input.value="";
+    const taskContent = document.createElement("div")
+    taskContent.appendChild(checkComplete())
+    const titleTask = document.createElement("span")
+    titleTask.classList.add("task")
+    titleTask.innerHTML=value;
+    taskContent.appendChild(titleTask)
+    task.appendChild(taskContent)
+    task.appendChild(deleteIcon())
+    list.appendChild(task)
+}
+
+btn.addEventListener("click",createTask);
+
+
